5d98f0ad64eb1f91f951d2a83974026550cc9b18
Sagar Kumar <117902138+prem95199@users.noreply.github.com>
2025-02-16 19:35:03 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+    $conn = mysqli_connect("localhost:3307", "root", "", "user") or die("connectionfailed");
+
+    if($conn)
+    {
+       // echo "connected ok";
+    }
+    else
+    {
+        echo "connection failed";
+    }
+
+?>
